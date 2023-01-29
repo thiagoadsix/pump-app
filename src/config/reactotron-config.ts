106f284eaf01234f6854d2dc0482f78ecaedcc77
Thiago Andrade Silva <thiago.adsix@gmail.com>
2023-01-29 18:54:03 -0300

    Refactor/pump 13 (#14)
    * refactor(workout): refactor the screen responsible to sets/workouts
    
    * chore(release): 1.1.2

--- src/config/reactotron-config.ts
+++ src/config/reactotron-config.ts
@@ -1,7 +1,7 @@
 import Reactotron from 'reactotron-react-native'
 
 if (__DEV__) {
-	Reactotron.configure({ host: '127.0.0.1', name: 'Pump' })
+	Reactotron.configure({ host: '192.168.1.11', name: 'Pump' })
 		.useReactNative()
 		.connect()
 }
