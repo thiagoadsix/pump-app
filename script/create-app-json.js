d2f94c4e0d7fd1a27bfa657f632a747ec2a9c481
Thiago Andrade Silva <thiago.adsix@gmail.com>
2023-03-20 16:26:35 -0300

    fix(app.json): update the script to app.json

--- script/create-app-json.js
+++ script/create-app-json.js
@@ -11,46 +11,49 @@ fs.writeFile(
 				adMobBannerId: process.env.AD_MOB_BANNER_ID,
 				adMobInterstitialId: process.env.AD_MOB_INTERSTITIAL_ID
 			},
-			name: 'pump',
-			slug: 'pump',
-			version: '1.0.0',
-			orientation: 'portrait',
-			icon: './assets/icon.png',
-			userInterfaceStyle: 'light',
-			splash: {
-				image: './assets/splash.png',
-				resizeMode: 'contain',
-				backgroundColor: '#ffffff',
+			'name': 'pump',
+			'slug': 'pump',
+			'version': '1.0.0',
+			'orientation': 'portrait',
+			'icon': './assets/icon.png',
+			'userInterfaceStyle': 'light',
+			'splash': {
+				'image': './assets/splash.png',
+				'resizeMode': 'contain',
+				'backgroundColor': '#ffffff'
 			},
-			updates: {
-				fallbackToCacheTimeout: 0,
-				enabled: true,
+			'updates': {
+				'fallbackToCacheTimeout': 0,
+				'enabled': true
 			},
-			assetBundlePatterns: ['**/*'],
-			ios: {
-				supportsTablet: true,
-				bundleIdentifier: 'com.mobile.pump',
+			'assetBundlePatterns': [
+				'**/*'
+			],
+			'ios': {
+				'supportsTablet': true,
+				'bundleIdentifier': 'com.mobile.pump'
 			},
-			android: {
-				adaptiveIcon: {
-					foregroundImage: './assets/adaptive-icon.png',
-					backgroundColor: '#FFFFFF',
+			'android': {
+				'adaptiveIcon': {
+					'foregroundImage': './assets/adaptive-icon.png',
+					'backgroundColor': '#FFFFFF'
 				},
-				package: 'com.mobile.pump',
+				'package': 'com.mobile.pump'
 			},
-			web: {
-				favicon: './assets/favicon.png',
+			'web': {
+				'favicon': './assets/favicon.png'
 			},
-			plugins: [
+			'plugins': [
 				[
 					'expo-build-properties',
 					{
-						ios: {
-							useFrameworks: 'static',
-						},
-					},
-				],
+						'ios': {
+							'useFrameworks': 'static'
+						}
+					}
+				]
 			],
+			'owner': 'thiago.adisx'
 		},
 		'react-native-google-mobile-ads': {
 			android_app_id: String(process.env.AD_MOB_ANDROID_API_ID),
